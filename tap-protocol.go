6107b9c912dddc4fb714d8a6aa36f897fb5e4577
Andreas Schjønhaug <andreas@schjonhaug.com>
2023-12-01 10:05:19 +0100

    Refactoring new

--- tap-protocol.go
+++ tap-protocol.go
@@ -153,68 +153,6 @@ func (tapProtocol *TapProtocol) certs() error {
 
 }
 
-func (tapProtocol *TapProtocol) New(cvc string) (int, error) {
-
-	tapProtocol.transport.Connect()
-	defer tapProtocol.transport.Disconnect()
-
-	return tapProtocol.new(cvc)
-
-}
-
-func (tapProtocol *TapProtocol) new(cvc string) (int, error) {
-
-	tapProtocol.status()
-
-	fmt.Println("------------")
-	fmt.Println("New")
-	fmt.Println("------------")
-
-	command := command{Cmd: "new"}
-
-	auth, err := tapProtocol.authenticate(cvc, command)
-
-	if err != nil {
-		fmt.Println(err)
-		return 0, err
-	}
-
-	newCommand := newCommand{
-		command: command,
-		Slot:    tapProtocol.activeSlot, //TODO check maximum
-		auth:    *auth,
-	}
-
-	data, err := tapProtocol.sendReceive(newCommand)
-
-	if err != nil {
-		return 0, err
-	}
-
-	switch data := data.(type) {
-	case NewData:
-
-		fmt.Println("#######")
-		fmt.Println("# NEW #")
-		fmt.Println("#######")
-
-		fmt.Println("Slot:             ", data.Slot)
-
-		tapProtocol.currentCardNonce = data.CardNonce
-		tapProtocol.activeSlot = data.Slot
-
-		return data.Slot, nil
-	case ErrorData:
-		fmt.Println("FOUND ERROR DATA")
-		return 0, errors.New(data.Error)
-
-	default:
-		return 0, errors.New("undefined error")
-
-	}
-
-}
-
 func (tapProtocol *TapProtocol) authenticate(cvc string, command command) (*auth, error) {
 
 	fmt.Println("\n########")
