198f2ea19bc211c15b9dd0b7b38cbba8a9a8536d
Andreas Schjønhaug <andreas@schjonhaug.com>
2023-12-11 23:03:46 +0100

    Added comments

--- apdu.go
+++ apdu.go
@@ -7,6 +7,8 @@ import (
 	"github.com/skythen/apdu"
 )
 
+// apduWrap takes any value, serializes it using CBOR, and wraps it into an APDU command.
+// It returns the byte representation of the APDU command or an error if something goes wrong.
 func apduWrap(value interface{}) ([]byte, error) {
 
 	cborSerialized, err := cbor.Marshal(value)
@@ -20,6 +22,8 @@ func apduWrap(value interface{}) ([]byte, error) {
 
 }
 
+// apduUnwrap takes a byte slice, tries to parse it as an APDU response, and returns the data field of the response.
+// It returns an error if the byte slice cannot be parsed as an APDU response.
 func apduUnwrap(value []byte) ([]byte, error) {
 
 	rapdu, err := apdu.ParseRapdu(value)
